0668d408347e39b289922dfd1199a5003f1bb2b3
Monir1231 <monirmolla324@gmail.com>
2025-06-02 23:52:15 +0600

    bank-site

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig ({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                careers: resolve(__dirname, "careers.html"),
+                about: resolve(__dirname, "about.html"),
+                 login: resolve(__dirname, "login.html"),
+                security: resolve(__dirname, "security.html"),
+                singup: resolve(__dirname, "singup.html")
+            },
+        },
+    },
+});
